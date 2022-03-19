0da63ac117dab605ff325bf592a522545482d503
JPHITA <jphita18@gmail.com>
2022-03-18 20:09:13 -0500

    Add files via upload

---
+++ src/main/java/Auto.java
@@ -0,0 +1,49 @@
+package test;
+
+public class Auto {
+
+	String modelo;
+	int precio;
+	Asiento[] asientos;
+	String marca;
+	Motor motor;
+	int registro;
+	
+	static int cantidadCreados;
+	
+	int cantidadAsientos() {
+	
+		int cAsientos = 0;
+		
+		for(int i = 0; i < this.asientos.length; i++) {
+			if (this.asientos[i] != null) {
+				cAsientos++;
+			}
+		}
+		
+		return cAsientos;
+	}
+	
+	String verificarIntegridad() {
+		
+		boolean original = true;
+		
+		if (this.registro != motor.registro) {
+			original = false;
+		}
+		
+		for(int i = 0; i < this.asientos.length; i++) {
+			if (this.registro != this.asientos[i].registro) {
+				original = false;
+				break;
+			}
+		}
+		
+		if (original) {
+			return "Las piezas no son originales";
+		} else {
+			return "Auto original";
+		}
+	}
+	
+}
